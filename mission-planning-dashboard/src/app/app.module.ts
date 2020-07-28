2160cb92c90536b47df7fabea5410df8dff88f34
Mission: St. Louis <mstl@Missions-MBP-45.attlocal.net>
2020-07-27 19:58:11 -0500

    Studio 1 complete

--- mission-planning-dashboard/src/app/app.module.ts
+++ mission-planning-dashboard/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CrewComponent } from './crew/crew.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { ExperimentComponent } from './experiment/experiment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CrewComponent,
+    EquipmentComponent,
+    ExperimentComponent
   ],
   imports: [
     BrowserModule
